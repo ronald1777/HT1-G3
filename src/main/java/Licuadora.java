3f4579a9116a4a88734a60c8e65400f1c5c2fbfc
angc <angcoder.c@gmail.com>
2025-01-16 11:47:07 -0600

    check full methods

--- src/main/java/Licuadora.java
+++ src/main/java/Licuadora.java
@@ -1,6 +1,17 @@
-package com;
 public class Licuadora {
+    int currentElements = 0;
     boolean state = false;
     boolean full = false;
     int speed = 0;
+
+    public void addElement () {
+        if (full) {
+            currentElements++;
+        }
+        full = currentElements > 5 ? true : false;
+    }
+
+    public boolean isFull () {
+        return full;
+    }
 }
